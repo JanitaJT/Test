f70dc37dbcd603deab2e739b757fd0416d5a4a1a
Janita <bgj369@myy.haaga-helia.fi>
2022-09-12 18:49:31 +0300

    test lisätty

---
+++ test/src/App.js
@@ -0,0 +1,11 @@
+import SubjectScreen from "./screens/SubjectScreen";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <SubjectScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ test/src/components/SubjectList.js
@@ -0,0 +1,157 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import { Typography } from "@mui/material";
+
+export default function SubjectList() {
+  const [subjectList, setSubjectList] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get/program").then((response) => {
+      setSubjectList(response.data);
+    });
+  }, []);
+
+  // STYLES
+  const Box = styled(Paper)(({ theme }) => ({
+    // maxHeight: "350px",
+    overflow: "auto",
+    // padding: "5px",
+  }));
+
+  return (
+    <div>
+      <Box>
+        <nav>
+          {subjectList.map((value) => {
+            return (
+              <List key={value.id}>
+                <ListItem disablePadding>
+                  <Grid item md={3} xs={7} padding={2}>
+                    <Typography
+                      variant="caption"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Name:
+                    </Typography>
+                    <ListItemText
+                      primary={value.subjectName}
+                      primaryTypographyProps={{
+                        variant: "body2",
+                      }}
+                    ></ListItemText>
+                  </Grid>
+                  <Grid item md={2} xs={3} padding={2}>
+                    <Typography
+                      variant="caption"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Group size:
+                    </Typography>
+                    <ListItemText
+                      primary={value.groupSize}
+                      primaryTypographyProps={{
+                        variant: "body2",
+                      }}
+                    ></ListItemText>
+                  </Grid>
+                  <Grid item md={2} xs={2} padding={2}>
+                    <Typography
+                      variant="caption"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Group count:
+                    </Typography>
+                    <ListItemText
+                      primary={value.groupCount}
+                      primaryTypographyProps={{
+                        variant: "body2",
+                      }}
+                    ></ListItemText>
+                  </Grid>
+                  <Grid item md={2} xs={2} padding={3}>
+                    <Typography
+                      variant="caption"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Session length:
+                    </Typography>
+                    <ListItemText
+                      primary={value.sessionLength}
+                      primaryTypographyProps={{
+                        variant: "body2",
+                      }}
+                    ></ListItemText>
+                  </Grid>
+                  <Grid item md={2} xs={2} padding={3}>
+                    <Typography
+                      variant="caption"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Session count:
+                    </Typography>
+                    <ListItemText
+                      primary={value.sessionCount}
+                      primaryTypographyProps={{
+                        variant: "body2",
+                      }}
+                    ></ListItemText>
+                  </Grid>
+                  <Grid item md={2} xs={2} padding={3}>
+                    <Typography
+                      variant="caption"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Area:
+                    </Typography>
+                    <ListItemText
+                      primary={value.area}
+                      primaryTypographyProps={{
+                        variant: "body2",
+                      }}
+                    ></ListItemText>
+                  </Grid>
+                  <Grid item md={2} xs={2} padding={3}>
+                    <Typography
+                      variant="caption"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Program id:
+                    </Typography>
+                    <ListItemText
+                      primary={value.programId}
+                      primaryTypographyProps={{
+                        variant: "body2",
+                      }}
+                    ></ListItemText>
+                  </Grid>
+                  <Grid item md={3} xs={7} padding={2}>
+                    <Typography
+                      variant="caption"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Program name:
+                    </Typography>
+                    <ListItemText
+                      primary={value.name}
+                      primaryTypographyProps={{
+                        variant: "body2",
+                      }}
+                    ></ListItemText>
+                  </Grid>
+                </ListItem>
+                <Divider />
+              </List>
+            );
+          })}
+        </nav>
+      </Box>
+    </div>
+  );
+}
